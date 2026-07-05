9ebe73bbd058c6fff05c700531bf2e190b5a879e
Devansh Kumar <devanshonline9087@gmail.com>
2026-07-05 16:01:37 +0530

    Time: 13 ms (6.02%), Space: 49.4 MB (14.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,33 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        List<int[]> ans = new ArrayList<>();
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int top = 1;
-        int n = intervals.length;
-        int arr[] = new int[2 * n];
-        arr[0] = intervals[0][0];
-        arr[1] = intervals[0][1];
-
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= arr[top]) {
-                arr[top] = Math.max(arr[top], intervals[i][1]);
+        for (int[] interval : intervals) {
+            if (ans.isEmpty() || ans.get(ans.size() - 1)[1] < interval[0]) {
+                ans.add(interval);
             } else {
-                top++;
-                arr[top] = intervals[i][0];
-                top++;
-                arr[top] = intervals[i][1];
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], interval[1]);
             }
         }
 
-        int rows = (top + 1) / 2;
-        int matrix[][] = new int[rows][2];
-        int j = 0;
-        for (int i = 0; i < rows; i++) {
-            matrix[i][0] = arr[j];
-            j++;
-            matrix[i][1] = arr[j];
-            j++;
-
-        }
-        return matrix;
+        return ans.toArray(new int[ans.size()][]);
     }
 }
