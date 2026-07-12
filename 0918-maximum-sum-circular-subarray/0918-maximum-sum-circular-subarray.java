d3a5aca1bbddf3f6e799a09a24512986911beddd
Devansh Kumar <devanshonline9087@gmail.com>
2026-07-13 00:39:10 +0530

    Time: 3 ms (99.09%), Space: 51.2 MB (41.53%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,32 +1,33 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int maxKadens=Integer.MIN_VALUE;
-        int minKadens=Integer.MAX_VALUE;
+        int maxKadens = Integer.MIN_VALUE;
+        int minKadens = Integer.MAX_VALUE;
         //min
-        int sum=0;
-        for(int i:nums){
-            sum+=i;
-            minKadens=Math.min(minKadens,sum);
-            if(sum>0){
-                sum=0;
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+            minKadens = Math.min(minKadens, sum);
+            if (sum > 0) {
+                sum = 0;
             }
         }
-        sum=0;
-        for(int i:nums){
-            sum+=i;
-            maxKadens=Math.max(maxKadens,sum);
-            if(sum<0){
-                sum=0;
+        sum = 0;
+        for (int i : nums) {
+            sum += i;
+            maxKadens = Math.max(maxKadens, sum);
+            if (sum < 0) {
+                sum = 0;
             }
         }
         //max
-        sum=0;
-        for(int i:nums){
-            sum+=i;
+        sum = 0;
+        for (int i : nums) {
+            sum += i;
         }
-        if(maxKadens<0) return maxKadens;
-        int circ=sum-minKadens;
-        return Math.max(circ,maxKadens);
+        if (maxKadens < 0)
+            return maxKadens;
+        int circ = sum - minKadens;
+        return Math.max(circ, maxKadens);
 
     }
 }
