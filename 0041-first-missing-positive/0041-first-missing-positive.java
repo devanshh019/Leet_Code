db43822133e77bcb19d9df974ed693e304308799
Devansh Kumar <devanshonline9087@gmail.com>
2026-07-16 21:23:56 +0530

    Time: 1 ms (100%), Space: 71.3 MB (47.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,19 +1,30 @@
 class Solution {
+
+    public void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int max = 0;
-        for (int i : nums) {
-            if (i > 0) {
-                set.add(i);
-                max = Math.max(max, i);
+        int n = nums.length, i = 0;
+
+        while (i < n) {
+            if (nums[i] > 0 &&
+                    nums[i] <= n &&
+                    nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
             }
         }
 
-        for (int i = 1; i <= max; i++) {
-            if (!set.contains(i)) {
-                return i;
+        for (int x = 0; x < n; x++) {
+            if (nums[x] - 1 != x) {
+                return x + 1;
             }
         }
-        return max + 1;
+
+        return n + 1;
     }
 }
