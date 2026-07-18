4fa2f266d3656a3315e87f09200a88ac09e9f929
Devansh Kumar <devanshonline9087@gmail.com>
2026-07-18 15:09:49 +0530

    Time: 0 ms (100%), Space: 47.7 MB (98.16%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans[]={-1,-1};
+        return new int[]{firstOcc(nums, target), lastOcc(nums, target)};
+    }
 
-        int l=0,h=nums.length-1;
+    private int firstOcc(int[] nums, int target) {
+        int l = 0, h = nums.length - 1;
+        int ans = -1;
 
-        while(l<=h){
-            int mid=(l+h)/2;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
 
-            if(nums[mid]==target){
-                int left=mid,right=mid;
-                while(left>=0 && nums[left]==target) left--;
-                while(right<nums.length&&nums[right]==target) right++;
-
-                ans[0]=left+1;
-                ans[1]=right-1;
-
-                break;
-            }
-            else if(nums[mid]>target){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
+            if (nums[mid] == target) {
+                ans = mid;
+                h = mid - 1; // keep searching left
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
+        return ans;
+    }
 
+    private int lastOcc(int[] nums, int target) {
+        int l = 0, h = nums.length - 1;
+        int ans = -1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                l = mid + 1; // keep searching right
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
         return ans;
     }
 }
