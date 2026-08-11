c1c278cbfb9d7dc12930353c6fda1c26ff9e7735
Devansh Kumar <devanshonline9087@gmail.com>
2026-08-11 20:03:33 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{firstOcc(nums, target), lastOcc(nums, target)};
-    }
+        int first=-1,last=-1;
 
-    private int firstOcc(int[] nums, int target) {
-        int l = 0, h = nums.length - 1;
-        int ans = -1;
+        int low=0,high=nums.length-1;
 
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
+        while(low<=high){ //first
+            int mid = low+(high-low)/2;
 
-            if (nums[mid] == target) {
-                ans = mid;
-                h = mid - 1; // keep searching left
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
+            if(nums[mid]==target){
+                high=mid-1;
+                first=mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return ans;
-    }
 
-    private int lastOcc(int[] nums, int target) {
-        int l = 0, h = nums.length - 1;
-        int ans = -1;
+        low=0;
+        high=nums.length-1;
 
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
-            if (nums[mid] == target) {
-                ans = mid;
-                l = mid + 1; // keep searching right
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
+            if(nums[mid]==target){
+                low=mid+1;
+                last=mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
+
+        int ans[]={first,last};
         return ans;
     }
 }
