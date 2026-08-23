dbaed0e368f3e3432b2e57db10c4cb1ddf50c1c4
Devansh Kumar <devanshonline9087@gmail.com>
2026-08-23 18:34:35 +0530

    Time: 2 ms (89.01%), Space: 47.7 MB (81.4%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        if(nums.length<=1) return true;
+        for(int i=0;i<nums.length-1;i++){
+            if(i>max) return false;
+            max=Math.max(max,i+nums[i]);
+            if(max>=nums.length-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
